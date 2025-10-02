12bb26c472f954066073feaf7be2e56506ec1e39
Robin Daviau <daviau.robin@gmail.com>
2025-10-02 13:17:47 +0200

    chore: format & security checks ok

--- main.tf
+++ main.tf
@@ -69,7 +69,6 @@ resource "google_compute_subnetwork" "subnet" {
   network                  = google_compute_network.vpc.id
   private_ip_google_access = true
 
-  # VPC Flow Logs
   log_config {
     aggregation_interval = "INTERVAL_5_SEC"
     flow_sampling        = 0.5
